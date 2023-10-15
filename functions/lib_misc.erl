8fcbff02b616f334c20a7f94a6de7c8f7fc536a5
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-14 21:35:05 -0700

    Some functions

--- functions/lib_misc.erl
+++ functions/lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([for/3, my_tuple_to_list / 1]).
+-export([for/3, my_tuple_to_list / 1, my_time_func/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I)|for(I + 1, Max, F)].
@@ -8,3 +8,9 @@ my_tuple_to_list(Tuple) -> my_tuple_to_list(Tuple, 1, tuple_size(Tuple)).
 
 my_tuple_to_list(Tuple, Pos, Size) when Pos =< Size -> [element(Pos, Tuple) | my_tuple_to_list(Tuple, Pos + 1, Size)];
 my_tuple_to_list(_Tuple, _Pos, _Size) -> [].
+
+my_time_func(F) ->
+    StartedAt = erlang:system_time(millisecond),
+    F,
+    FinishedAt = erlang:system_time(millisecond),
+    FinishedAt - StartedAt.
