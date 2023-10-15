d1b1cce825e98e97da6a9cb09bef8c650455a6ce
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-14 21:25:45 -0700

    My tuple to list

--- functions/lib_misc.erl
+++ functions/lib_misc.erl
@@ -1,5 +1,10 @@
 -module(lib_misc).
--export([for/3]).
+-export([for/3, my_tuple_to_list / 1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I)|for(I + 1, Max, F)].
+
+my_tuple_to_list(Tuple) -> my_tuple_to_list(Tuple, 1, tuple_size(Tuple)).
+
+my_tuple_to_list(Tuple, Pos, Size) when Pos =< Size -> [element(Pos, Tuple) | my_tuple_to_list(Tuple, Pos + 1, Size)];
+my_tuple_to_list(_Tuple, _Pos, _Size) -> [].
