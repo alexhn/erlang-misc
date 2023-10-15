8a5aa92c50436a764fc60e2ec8fab35f5f4574d8
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-14 21:14:14 -0700

    More geometry

--- geometry/geometry.erl
+++ geometry/geometry.erl
@@ -1,5 +1,11 @@
 -module(geometry).
--export([area/1]).
+-export([area/1, perimeter/1]).
 
 area({rectangle, Width, Height}) -> Width * Height;
+area({circle, Radius}) -> math:pi() * Radius * Radius;
+area({rightangle_triangle, Side1, Side2}) -> Side1 * Side2 / 2.0;
 area({square, Side}) -> Side * Side.
+
+perimeter({rectangle, Width, Height}) -> 2 * (Width + Height);
+perimeter({square, Side}) -> 4 * Side;
+perimeter({circle, Radius}) -> 2.0 * math:pi() * Radius.
