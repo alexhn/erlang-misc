6e28d02cb3494c9ac9cee410f55a0a4cc8a96d33
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-21 03:22:43 -0700

    for loop

--- tutorials/loops.erl
+++ tutorials/loops.erl
@@ -7,6 +7,14 @@ while([_|T], Acc) ->
     io:fwrite("~w~n", [Acc]),
     while(T, Acc + 1).
 
+for(0,_) ->
+    [];
+
+for(N,Term) when N > 0 ->
+    io:fwrite("Hello~n"),
+    for(N - 1, Term).
+
 test1() ->
     X = [1, 2, 3, 4],
-    while(X).
+    while(X),
+    for(5,1).
