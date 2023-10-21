c77ad2f51565cd30fb14de0b93ea9dee4f407f5c
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-21 03:05:22 -0700

    tests

--- tutorials/helloworld.erl
+++ tutorials/helloworld.erl
@@ -1,7 +1,28 @@
 % hello world
 -module(helloworld).
 -import(io, [fwrite/1]).
--export([start/0]).
+-export([start/0, test_bool/0, test_binary/0, test_tuple/0, test_map/0, test_list/0]).
+
+test_list() ->
+    L = [10, 20, 30],
+    io:fwrite("~w", [length(L)]).
+
+test_map() ->
+    M1 = #{name=>john, age=>25},
+    io:fwrite("~w", [map_size(M1)]).
+
+test_tuple() ->
+    P = {john, 24, {june, 25}},
+    io:fwrite("~w", [tuple_size(P)]).
+
+test_binary() ->
+    Bin1 = <<10, 20>>,
+    X = binary_to_list(Bin1),
+    io:fwrite("~w", [X]).
+
+test_bool() ->
+    io:fwrite(2 =< 3),
+    io:fwrite("~w", [1+1]).
 
 start() ->
-    fwrite("Hello, world!\n").
+    io:fwrite("Hello, world!\n").
