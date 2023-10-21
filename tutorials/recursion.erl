f2a22e11ac5d3b65843d00a4ed554101879ba2fb
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-21 13:44:50 -0700

    tail reverse

--- tutorials/recursion.erl
+++ tutorials/recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([factorial_test/0, len/1, tail_len/1]).
+-export([factorial_test/0, len/1, tail_len/1, duplicate/2, tail_reverse/1]).
 
 
 tail_len(L) -> tail_len(L, 0).
@@ -11,6 +11,17 @@ len([_|T]) -> 1 + len(T).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 -> N * fac(N - 1).
+
+duplicate(0,_) ->
+    [];
+
+duplicate(N,Term) when N > 0 ->
+    io:fwrite("~w~n", [Term]),
+    [Term|duplicate(N - 1, Term)].
+
+tail_reverse(L) -> tail_reverse(L, []).
+tail_reverse([], Acc) -> Acc;
+tail_reverse([H|T], Acc) -> tail_reverse(T,[H|Acc]).
 
 factorial_test() ->
     X = fac(4),
