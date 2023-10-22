80bf81169d27e596b4ffbaa50725bb0d3d358eb1
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-21 22:45:37 -0700

    Maps

--- tutorials/mapstest.erl
+++ tutorials/mapstest.erl
@@ -1,5 +1,15 @@
 -module(mapstest).
--export([test1/0, test2/0]).
+-export([test1/0, test2/0, test3/0, test4/0]).
+
+test4() ->
+    Lst1 = [{"a", 1}, {"b", 2}, {"c", 3}],
+    Map1 = maps:from_list(Lst1),
+    io:fwrite("~p~n", [maps:find("a", Map1)]).
+
+test3() ->
+    Lst1 = [{"a", 1}, {"b", 2}, {"c", 3}],
+    Map1 = maps:from_list(Lst1),
+    io:fwrite("~p~n", [maps:find("a", Map1)]).
 
 test2() ->
     Lst1 = [{"a", 1}, {"b", 2}, {"c", 3}],
