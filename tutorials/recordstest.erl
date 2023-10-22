dfec7aed1fe710451065d5be5fe540fe090d1e39
Aleksandr Nikiforov <alex@alexn.ee>
2023-10-21 22:54:15 -0700

    records

--- tutorials/recordstest.erl
+++ tutorials/recordstest.erl
@@ -1,6 +1,8 @@
 -module(recordstest).
 -record(person, {name = "", id}).
 -export([test1/0]).
+-record(employee, {person, salary}).
+
 
 test1() ->
     P = #person{name = "John", id = 1},
@@ -10,4 +12,7 @@ test1() ->
 
     P1 = P#person{name = "Dan"},
     io:fwrite("~p~n", [P1#person.id]),
-    io:fwrite("~p~n", [P1#person.name]).
+    io:fwrite("~p~n", [P1#person.name]),
+
+    E1 = #employee{person = #person{name = "John", id = 1}, salary=2000},
+    io:fwrite("~p~n", [E1#employee.person#person.name]).
